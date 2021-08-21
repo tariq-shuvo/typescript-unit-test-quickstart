eb501735619c978c6c28229061d738892089c377
Tariqul Islam <tariq10043@gmail.com>
2021-08-21 18:58:44 +0600

    Fill up instructions

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Assessments
 
+### npm run server
+
+To application run using https://localhost:8080 for development
+
+### npm run build
+
+To build the application for production
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: path.join(__dirname, 'app', 'index'),
   watch: true,
   output: {
